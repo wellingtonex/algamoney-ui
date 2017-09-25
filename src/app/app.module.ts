01b46fe60e3c1849721682a1fe76b0015fa550ee
wellington.siqueira <wellingtonex@gmail.com>
2017-09-25 20:44:38 -0300

    corrigindo instalação do font-awesome, configurando e testando tudo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {TabViewModule} from 'primeng/primeng';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TabViewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
