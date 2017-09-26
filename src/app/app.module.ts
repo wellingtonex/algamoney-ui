e38e7d9a66ba9f3845f5b430d6955a8b4f9e2045
wellington.siqueira <wellingtonex@gmail.com>
2017-09-25 21:39:53 -0300

    criando o datatable de lancamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import {InputTextModule} from 'primeng/components/inputtext/inputtext';
 import {ButtonModule} from 'primeng/components/button/button';
+import {DataTableModule} from 'primeng/components/datatable/datatable';
 
 import { AppComponent } from './app.component';
 
@@ -14,7 +15,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     InputTextModule,
-    ButtonModule
+    ButtonModule,
+    DataTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
