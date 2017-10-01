8b84521b30b8b71a605def4299fc6262cdbba451
wellington.siqueira <wellingtonex@gmail.com>
2017-10-01 18:05:34 -0300

    parametrizando valores de propriedades da diretiva com @Input

--- src/app/campo-colorido.directive.ts
+++ src/app/campo-colorido.directive.ts
@@ -1,14 +1,16 @@
-import {Directive, HostListener, HostBinding} from '@angular/core';
+import {Directive, HostListener, HostBinding, Input} from '@angular/core';
 
 @Directive({
   selector: '[appCampoColorido]'
 })
 export class CampoColoridoDirective {
 
+  @Input() cor = 'gray';
+
   @HostBinding('style.backgroundColor') corDeFundo: string;
 
   @HostListener('focus') aoGanharFoco() {
-    this.corDeFundo = 'yellow';
+    this.corDeFundo = this.cor;
   }
 
   @HostListener('blur') aoPerderFocoFoco() {
