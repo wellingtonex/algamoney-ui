9ee29c2bf21bf92dfdbf574d8495c78ab19a350d
wellington.siqueira <wellingtonex@gmail.com>
2017-10-10 22:26:41 -0300

    adicionando validações no formulario de cadastro de lancamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import {InputTextModule} from 'primeng/components/inputtext/inputtext';
 import {ButtonModule} from 'primeng/components/button/button';
@@ -36,6 +37,7 @@ import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.compo
   imports: [
     BrowserModule,
     InputTextModule,
+    FormsModule,
     ButtonModule,
     DataTableModule,
     TooltipModule,
