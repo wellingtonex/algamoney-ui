cae8048b2e61f5fda462bbc10d889ab7cdf6473d
wellington.siqueira <wellingtonex@gmail.com>
2017-10-14 13:05:31 -0300

    criando o modulo core e movendo o componente navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+
+import { CoreModule } from './core/core.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
 
     LancamentosModule,
-    PessoasModule
+    PessoasModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
