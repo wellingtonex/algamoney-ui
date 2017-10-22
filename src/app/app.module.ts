d9a1561be774e5604cdb59f284d4c0e3a5ce937a
wellington.siqueira <wellingtonex@gmail.com>
2017-10-22 16:31:02 -0300

    integrando algamoney-ui com algamoney-api, obtendo lancametos da api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
 import { CoreModule } from './core/core.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
+
+import { LancamentoService } from './lancamentos/lancamento.service';
 
 @NgModule({
   declarations: [
@@ -16,12 +19,12 @@ import { LancamentosModule } from './lancamentos/lancamentos.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-
+    HttpModule,
     LancamentosModule,
     PessoasModule,
     CoreModule
   ],
-  providers: [],
+  providers: [LancamentoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
