a239536a15428bd378b6e841eb18ba1149379731
wellington.siqueira <wellingtonex@gmail.com>
2017-10-25 22:18:40 -0300

    implementando exclusão de lancamentos

--- src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,5 +1,5 @@
 import { LancamentoService, LancamentoFiltro } from './../lancamento.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { LazyLoadEvent } from 'primeng/components/common/api';
 
 @Component({
@@ -12,6 +12,7 @@ export class LancamentosPesquisaComponent implements OnInit {
   lancamentos = [];
   filtro = new LancamentoFiltro();
   totalRegistro = 0;
+  @ViewChild('tabela') grid;
 
   constructor(private lancamentoService: LancamentoService) { }
 
@@ -29,5 +30,12 @@ export class LancamentosPesquisaComponent implements OnInit {
   mudarPagina(event: LazyLoadEvent) {
     const pagina = event.first / event.rows;
     this.pesquisar(pagina);
+  }
+
+  excluir(lancamento: any) {
+    this.lancamentoService.excluir(lancamento.codigo)
+      .then(() => {
+        this.grid.first = 0;
+      });
   }
 }
