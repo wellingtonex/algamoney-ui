7bc831b833531642e52167fe9a5e3a1ca393670e
wellington.siqueira <wellingtonex@gmail.com>
2017-10-30 21:59:06 -0300

    configurando o locale da aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -30,7 +30,11 @@ import { ConfirmationService } from 'primeng/components/common/api';
     ToastyModule.forRoot(),
     ConfirmDialogModule
   ],
-  providers: [LancamentoService, ConfirmationService],
+  providers: [
+    LancamentoService,
+    ConfirmationService,
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
