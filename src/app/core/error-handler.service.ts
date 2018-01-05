f6d1da474edbbb021d82cd1755a2bee951d684bc
wellington.siqueira <wellingtonex@gmail.com>
2018-01-05 00:17:31 -0200

    melhorando tratamento de erros

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,5 +1,7 @@
-import { ToastyService } from 'ng2-toasty';
 import { Injectable } from '@angular/core';
+import { Response } from '@angular/http';
+
+import { ToastyService } from 'ng2-toasty';
 
 @Injectable()
 export class ErrorHandlerService {
@@ -11,11 +13,26 @@ export class ErrorHandlerService {
 
     if (typeof errorResponse === 'string') {
       msg = errorResponse;
+
+    } else if (errorResponse instanceof Response
+        && errorResponse.status >= 400 && errorResponse.status <= 499) {
+      let errors;
+      msg = 'Ocorreu um erro ao processar a sua solicitação';
+
+      try {
+        errors = errorResponse.json();
+
+        msg = errors[0].mensagemUsuario;
+      } catch (e) { }
+
+      console.error('Ocorreu um erro', errorResponse);
+
     } else {
-      msg = 'Erro ao processar serviço remoto. Tente novamento.';
-      console.log(errorResponse);
+      msg = 'Erro ao processar serviço remoto. Tente novamente.';
+      console.error('Ocorreu um erro', errorResponse);
     }
 
     this.toasty.error(msg);
   }
+
 }
